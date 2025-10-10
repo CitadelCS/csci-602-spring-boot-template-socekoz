144e59bcd56235a1dd8fba5d486cd63243c7ffa8
socek <socekoz@outlook.com>
2025-10-10 16:55:15 -0400

    commit

---
+++ src/test/java/edu/citadel/main/SpringIntegrationTest.java
@@ -0,0 +1,23 @@
+package edu.citadel.main;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@CucumberContextConfiguration
+@SpringBootTest
+
+public class SpringIntegrationTest {
+    @LocalServerPort
+    private int port;
+    protected TestRestTemplate testRestTemplate = new TestRestTemplate();
+    protected ResponseEntity<String> responseEntity;
+    protected void executeGet(String url) {
+        responseEntity = testRestTemplate.getForEntity(url, String.class);
+    }
+    protected String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+}
